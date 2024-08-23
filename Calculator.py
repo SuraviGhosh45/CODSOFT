e4501145b6f9ef325c7e3f3c7134e5a1288183bc
Suravi  Ghosh <163166391+SuraviGhosh45@users.noreply.github.com>
2024-08-24 04:22:48 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,25 +1,35 @@
-print("1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n5. Division & remainder")
-choice = int(input("Enter Choice: "))
-num1 = float(input("Enter 1st Number: "))
-num2 = float(input("Enter 2nd Number: "))
-match choice:
-    case 1:
+while True:
+    print("1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n5. Division & remainder\n6. Exit")
+    choice = int(input("Enter Choice: "))
+    if choice == 1:
+        num1 = float(input("Enter 1st Number: "))
+        num2 = float(input("Enter 2nd Number: "))
         print("After Adding Two Numbers:", num1 + num2)
-    case 2:
+    elif choice == 2:
+        num1 = float(input("Enter 1st Number: "))
+        num2 = float(input("Enter 2nd Number: "))
         print("After Subtracting Two Numbers:", num1 - num2)
-    case 3:
+    elif choice == 3:
+        num1 = float(input("Enter 1st Number: "))
+        num2 = float(input("Enter 2nd Number: "))
         print("After Multiplying Two Numbers:", num1 * num2)
-    case 4:
+    elif choice == 4:
+        num1 = float(input("Enter 1st Number: "))
+        num2 = float(input("Enter 2nd Number: "))
         if num2 != 0:
             print("After Dividing Two Numbers, The Division is:", num1 / num2)
         else:
             print("Division by zero is not allowed.")
-    case 5:
+    elif choice == 5:
+        num1 = float(input("Enter 1st Number: "))
+        num2 = float(input("Enter 2nd Number: "))
         if num2 != 0:
             print("After Dividing Two Numbers, The Division is:", num1 / num2)
             print("And The remainder is:", num1 % num2)
         else:
             print("Division by zero is not allowed.")
-    case _:
-        print("Invalid choice. Please select a valid option.")
-print("Thanks For Visiting!")
+    elif choice == 6:
+            print("Thanks For Visiting!")
+            break
+    else:
+        print("Invalid choice.")
